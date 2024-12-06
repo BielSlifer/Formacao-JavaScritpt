8ac04462be968c704181234e0c601fa62684d488
BielSlifer <biel161760redes@gmail.com>
2024-12-06 18:15:35 -0300

    conteúdo adicionado

--- Funções/OrganizarFuncoes.js
+++ Funções/OrganizarFuncoes.js
@@ -1,2 +1,11 @@
 //para padronizar, sempre criar uma function como main e executar o código principal nela
 //criar demais funções necessárias e chamar dentro da função main
+
+function calcularJuros() {
+    
+}
+
+function main () {
+    console.log("Programa principal");
+    calcularJuros();
+}
