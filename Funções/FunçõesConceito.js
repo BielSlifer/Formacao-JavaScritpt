81773e35f4a396c4c835eae9323578549ddf70b2
BielSlifer <biel161760redes@gmail.com>
2024-12-06 18:12:53 -0300

    mudança feita no arquivo

--- Funções/FunçõesConceito.js
+++ Funções/FunçõesConceito.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+// Funções em JS
+
+function sayMyName (name) {
+    console.log("Seu nome é: " + name);
+}
+sayMyName("Gabriel");
+
+//funções por padrão executam o código sem devolver nada, você pode pedir para retornar
+
+function quadrado(valor) {
+    return valor * valor
+}
+
+const quadradoDeDez = quadrado(2)
+console.log(quadradoDeDez);
+
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrescimo = (percentualJuros / 100) * valor
+    return valor + valorDeAcrescimo
+}
+console.log(incrementarJuros(100, 10));
+console.log(incrementarJuros(100, 15));
---
+++ Funções/OrganizarFuncoes.js
@@ -0,0 +1,2 @@
+//para padronizar, sempre criar uma function como main e executar o código principal nela
+//criar demais funções necessárias e chamar dentro da função main
